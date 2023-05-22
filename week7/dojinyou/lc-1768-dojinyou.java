965447bc2774aa86c9bd990b41bacf17b832bb77
유도진 <djyou128@gmail.com>
2023-05-22 09:47:19 +0900

    [7주차]알고리즘 문제 풀이(유도진) (#44)

---
+++ week7/dojinyou/lc-1768-dojinyou.java
@@ -0,0 +1,16 @@
+public class MergeStringsAlternately {
+	public String mergeAlternately(String word1, String word2) {
+		int maxLength = Math.max(word1.length(), word2.length());
+		StringBuilder sb = new StringBuilder(maxLength);
+		for(int i = 0; i < maxLength; i++) {
+			if (i < word1.length()) {
+				sb.append(word1.charAt(i));
+			}
+			if (i < word2.length()) {
+				sb.append(word2.charAt(i));
+			}
+		}
+
+		return sb.toString();
+	}
+}
